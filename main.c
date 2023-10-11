f8be5148347eba3c369b8d8011aac35c87f3bb10
Uzimonkey123 <normanbabiak@gmail.com>
2023-10-11 14:01:05 +0200

    Adding basic main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ *  @file main.c
+ * 
+ *  @author
+ *  @author Norman Babiak (xbabia01)
+ *  @author
+ *  @author
+ */
+
+// BASIC MAIN WITH CORRECT RETURN ON THE END OF PROGRAM
+
+#include "error.h"
+
+int main() {
+
+    return error;
+}
