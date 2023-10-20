9ee092d98c05ba3e040a6b9c93172d2d14414499
Milan Jakubec <jakubeclp@gmail.com>
2023-10-20 14:04:17 +0200

    defined a structure scanner_t

--- scanner.h
+++ scanner.h
@@ -1,11 +1,23 @@
 /**
- *  scanner.h and shit
+ *  @file scanner.h
  * 
  * 
- * @authors Jakub Ráček (xracek12)
+ *  @author Jakub Ráček (xracek12)
+ *  @author Milan Jakubec (xjakub41)
  */
 
 #include "token.h"
+
+// Enum for all the states of the FSM
+typedef enum scanner_state {
+    // Type out all the FSM states
+} scanner_state_t;
+
+// Scanner structure itself
+typedef struct{
+    enum fsm_curr_state state; // Current state the FSM is in
+
+} scanner_t;
 
 /**
  * @brief Function get_next_token() reads a token from stdin and returns it to be used by parser
@@ -13,8 +25,6 @@
  * @param stuff not sure jestli je potreba parametr? je to na tobe Jirko!
  * @returns a token from stdin
 */
-token_t* get_next_token(stuff idk);
+token_t get_next_token(void);
 
-typedef enum scanner_state {
-    // all the FSM states
-} scanner_state_t;
+// Missing functions to initialize and destroy the scanner
