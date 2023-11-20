298dcdab6907dc2fa5c15900fe7148dc17222a8c
Milan Jakubec <jakubeclp@gmail.com>
2023-11-20 16:07:06 +0100

    generator files added

---
+++ gen.c
@@ -0,0 +1,8 @@
+/**
+ *  @file gen.c
+ *
+ *
+ *  @authors Milan Jakubec (xjakub41)
+ *  @authors Jakub Ráček (xracek12)
+ *  @authors Norman Babiak (xbabia01)
+ */
---
+++ gen.h
@@ -0,0 +1,19 @@
+/**
+ *  @file gen.h
+ *
+ *
+ *  @authors Milan Jakubec (xjakub41)
+ *  @authors Jakub Ráček (xracek12)
+ *  @authors Norman Babiak (xbabia01)
+ */
+
+#include "str.h"
+#include "symtable.h"
+#include "token.h"
+
+typedef struct{
+    mystring_t something;
+
+
+
+} gen_t;
